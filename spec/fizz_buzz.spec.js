3e2407b2822d0a1ac3c8659ce7412b8903fbbbb8
GIL3512 <gyllia.ilamboalulea@viseo.com>
2016-08-13 18:35:19 +0200

    Refractor of function multiple_3_et_5 to add v1 to v2

--- spec/fizz_buzz.spec.js
+++ spec/fizz_buzz.spec.js
@@ -4,5 +4,7 @@ describe("Les nombres affichés", function() {
     it("La liste", function() {
 
         expect(multiple_3_et_5()).toContain("Fizz");
+        expect(multiple_3_et_5()).toEqual("FizzBuzz");
+        expect(multiple_3_et_5()).toBe("Buzz");
     });
 });
--- src/fizz_buzz.js
+++ src/fizz_buzz.js
@@ -1,18 +1,56 @@
-var tableau = [];
-
 function multiple_3_et_5() {
+    var fizz = false;
+    var buzz = false;
     for (var i = 1; i <= 100; i++) {
         if (i % 3 === 0) {
-            tableau[i-1] = "Fizz";
+            fizz = true;
         }
+        if (i % 5 === 0) {
+            buzz = true;
+        }
+        if (fizz && buzz)
+        {
+            document.write("FizzBuzz");
+            document.write("<br>");
+            
+            console.log("FizzBuzz");
+            console.log("<br>");
 
-        if (i % 5 === 0){
-            tableau[i-1] = "Buzz";
-        }else{
-            tableau[i]=i;
         }
+        else
+        {
+            if (fizz)
+            {
+                document.write("Fizz");
+                document.write("<br>");
+                
+                console.log("Fizz");
+                console.log("<br>");
+            }
+            else
+            {
+                if (buzz)
+                {
+                    document.write("Buzz");
+                    document.write("<br>");
+                    
+                    console.log("FizzBuzz");
+                    console.log("<br>");
+                }
+                else
+                {
+                    document.write(i);
+                    document.write("<br>");
+                    
+                    console.log(i);
+                    console.log("<br>");
+                }
+            }
+        }
+        fizz = false;
+        buzz = false;
+        
     }
-    return tableau;
 }
 multiple_3_et_5();
 
